c4adba6d1ab51af8bc4fa779ac6632629e5ade61
hamzadastagir <hamza.dastagir@gmail.com>
2025-02-04 01:14:54 +0500

    Added github workflow to deploy react

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/", // Use "/" because your site is hosted at the domain root
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
